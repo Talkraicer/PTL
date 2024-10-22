68a821fb6605b261f37c20ed484d718e6a7ed46e
Talkraicer <talkraicer@gmail.com>
2024-10-22 05:19:18 +0300

    Results Parsing

--- results/parse_exp_results.py
+++ results/parse_exp_results.py
@@ -8,7 +8,9 @@ class ResultsParser:
     def __init__(self, exp_file, period=60):
         self.tripinfo_file = exp_file + "_tripinfo.xml"
         self.lanes_file = exp_file + "_lanes.xml"
-        parts = split_all_parts(self.tripinfo_file)
+        parts = split_all_parts(exp_file)
+        print(exp_file)
+        print(parts)
         self.policy_name = parts[-1]
         self.seed = int(parts[-2])
         self.av_rate = float(parts[-3])
