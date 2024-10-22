477a592882db8b84490c90fd26cf9b3d1a5cd374
Talkraicer <talkraicer@gmail.com>
2024-10-22 05:05:02 +0300

    Results Parsing

--- Results/parse_all_results.py
+++ Results/parse_all_results.py
@@ -49,30 +49,33 @@ def create_metric_results_table(results_parsers, metric, vType=False):
     :param results_parsers: a list of ResultsParser objects
     :param metric: the metric to calculate the mean and std for
     :param vType: weather to calculate the metric for each vehicle type
-    :return: a dataframe of columns av_rate, (optional) subcolumns of vTypes,
+    :return: a dataframe of columns demand, subcolumns of av_rates, (optional) subcolumns of vTypes,
                 subcolumns mean and std, and rows of policies
     """
+    demands = list(set(map(lambda x: x.demand_name, results_parsers)))
     av_rates = list(set(map(lambda x: x.av_rate, results_parsers)))
     policies = list(set(map(lambda x: x.policy_name, results_parsers)))
+
     if vType:
         vTypes = ["AV", "HD", "Bus"]
-        df = pd.DataFrame(index=policies, columns=pd.MultiIndex.from_product([av_rates, vTypes, ["mean", "std"]]))
+        df = pd.DataFrame(index=policies, columns=pd.MultiIndex.from_product([demands,av_rates, vTypes, ["mean", "std"]]))
     else:
-        df = pd.DataFrame(index=policies, columns=pd.MultiIndex.from_product([av_rates, ["mean", "std"]]))
-    for policy in policies:
-        policy_parsers = list(filter(lambda x: x.policy_name == policy, results_parsers))
-        for av_rate in av_rates:
-            av_rate_parsers = list(filter(lambda x: x.av_rate == av_rate, policy_parsers))
-            if vType:
-                for vType in vTypes:
-                    vtype_results = calc_metric_over_simulations(av_rate_parsers, metric, vType)
-                    df.loc[policy, (av_rate, vType, "mean")] = vtype_results["mean"]
-                    df.loc[policy, (av_rate, vType, "std")] = vtype_results["std"]
-            else:
-                df.loc[policy, (av_rate, "mean")] = calc_metric_over_simulations(av_rate_parsers, metric)["mean"]
-                df.loc[policy, (av_rate, "std")] = calc_metric_over_simulations(av_rate_parsers, metric)["std"]
-    return df
+        df = pd.DataFrame(index=policies, columns=pd.MultiIndex.from_product([demands,av_rates, ["mean", "std"]]))
+    for demand in demands:
+        for policy in policies:
+            for av_rate in av_rates:
+                av_rate_parsers = list(filter(lambda x: x.av_rate == av_rate and x.demand_name == demand
+                                                        and x.policy_name == policy, results_parsers))
+                if vType:
+                    for vType in vTypes:
+                        vtype_results = calc_metric_over_simulations(av_rate_parsers, metric, vType)
+                        df.loc[policy, (av_rate, vType, "mean")] = vtype_results["mean"]
+                        df.loc[policy, (av_rate, vType, "std")] = vtype_results["std"]
+                else:
+                    df.loc[policy, (av_rate, "mean")] = calc_metric_over_simulations(av_rate_parsers, metric)["mean"]
+                    df.loc[policy, (av_rate, "std")] = calc_metric_over_simulations(av_rate_parsers, metric)["std"]
+        return df
 
 if __name__ == '__main__':
-    parsers = get_all_results_parsers("../SUMO/outputs")
+    parsers = get_all_results_parsers("SUMO/outputs")
     create_metric_results_table(parsers, "passDelay", vType=True).to_csv("passDelay.csv")
